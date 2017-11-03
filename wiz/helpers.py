789b008ff42a41be4af71efa34587f3c493ec77c
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-11-03 13:24:59 +0200

    Update create_license_file function

--- wiz/helpers.py
+++ wiz/helpers.py
@@ -8,12 +8,16 @@ DEFAULT_LICENCES = {
 }
 
 
-def create_license_file(license_name: str) -> str:
+def create_license_file(license_name: str, author_name=None) -> str:
     """"
     :param license_name: An license name
+    :param author_name: An author name, will added in an license file
     :return: An content of license
     """
     license_content = DEFAULT_LICENCES.get(license_name)
     if license_content is None:
         raise LicenseError
-    return license_content
+
+    author_name = author_name if author_name else 'Change me!'
+    out_put_license = license_content.replace('Wizi Generator', author_name)
+    return out_put_license
